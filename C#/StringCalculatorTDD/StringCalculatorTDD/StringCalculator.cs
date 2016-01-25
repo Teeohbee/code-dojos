15bbcff689b05a1d2dd54214c79c34efd9dd5e4f
Toby Clarke <Toby.Clarke@euromoneyplc.com>
2016-01-25 13:17:52 +0000

    string calculator dojo added

---
+++ C#/StringCalculatorTDD/StringCalculatorTDD/StringCalculator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculatorTDD
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers)) return 0;
+
+            List<string> stringSeparators = new List<string>() {",", "\n"};
+
+            if (numbers.StartsWith("//"))
+            {
+                stringSeparators = new List<string>() {numbers[2].ToString()};
+                numbers = numbers.Substring(4);
+            }
+
+            string[] splitNumbers = numbers.Split(stringSeparators.ToArray(), StringSplitOptions.None);
+            var sum = Sum(splitNumbers);
+            var negativeNumbers = NegativeNumbers(splitNumbers);
+            if (negativeNumbers.Any())
+            {
+                string[] negativeNumbersArray = negativeNumbers.ToArray();
+                throw new ArgumentException("negatives not allowed: " + string.Join(", ", negativeNumbersArray));
+            }
+            return sum;
+        }
+
+        private static List<string> NegativeNumbers(string[] splitNumbers)
+        {
+            List<string> negativeNumbers = new List<string>();
+            foreach (string number in splitNumbers)
+            {
+                if (number[0].Equals('-'))
+                {
+                    negativeNumbers.Add(number);
+                }
+            }
+            return negativeNumbers;
+        }
+
+        private static int Sum(string[] splitNumbers)
+        {
+            int sum = 0;
+            foreach (string number in splitNumbers)
+            {
+                sum += int.Parse(number);
+            }
+            return sum;
+        }
+    }
+}
---
+++ C#/StringCalculatorTDD/StringCalculatorTDD/StringCalculatorTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace StringCalculatorTDD
+{
+    [TestFixture]
+    class StringCalculatorTests
+    {
+        public StringCalculator Calculator;
+
+        [SetUp]
+        public void Init()
+        {
+            Calculator = new StringCalculator();
+        }
+
+        [Test]
+        public void ShouldReturnZeroWhenGivenNoNumbers()
+        {
+            Assert.AreEqual(0, Calculator.Add(""));
+        }
+        [Test]
+        public void ShouldReturnOneWhenGivenTheStringOne()
+        {
+            Assert.AreEqual(1, Calculator.Add("1"));
+        }
+        [Test]
+        public void ShouldReturnTwoWhenGivenTheStringTwo()
+        {
+            Assert.AreEqual(2, Calculator.Add("2"));
+        }
+        [Test]
+        public void ShouldReturnThreeWhenGivenTheStringOneAndTwo()
+        {
+            Assert.AreEqual(3, Calculator.Add("1,2"));
+        }
+        [TestCase("1,2,3", 6)]
+        [TestCase("9,3,7,4", 23)]
+        [TestCase("3,7,9,7,4", 30)]
+        [TestCase("2,5,7,9,3,10", 36)]
+        public void ShouldHandleAnUnknownAmountOfNumbers(string input, int expected)
+        {
+            Assert.AreEqual(expected, Calculator.Add(input));
+        }
+        [Test]
+        public void ShouldHandleNewLinesBetweenNumbers()
+        {
+            Assert.AreEqual(6, Calculator.Add("1\n2,3"));
+        }
+        [Test]
+        public void ShouldSupportDeclarationOfDifferentDelimiters()
+        {
+            Assert.AreEqual(3, Calculator.Add("//;\n1;2"));
+        }
+        [Test]
+        [ExpectedException( typeof( ArgumentException ), ExpectedMessage="negatives not allowed: -1, -4" )]
+        public void ShouldThrowExceptionWhenGivenNegativeNumbers()
+        {
+           Calculator.Add("1,-1,-4,6,7");
+        }
+    }
+}
