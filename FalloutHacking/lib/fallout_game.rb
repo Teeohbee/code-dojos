052b218250c4d4215d040fcde3c38c2e482ea9d8
Toby Clarke <teeohbee@gmail.com>
2016-01-31 18:25:14 +0000

    initial tests passing

--- FalloutHacking/lib/fallout_game.rb
+++ FalloutHacking/lib/fallout_game.rb
@@ -1,7 +1,23 @@
 class FalloutGame
+attr_accessor :correct_answer
+attr_accessor :correct_letters
 
-def guess
-  
+def initialize()
+    @correct_answer = ""
+    @correct_letters = 0
+  end
+
+def guess(answer)
+  if answer == correct_answer
+    true
+  else
+    answer.each_char.with_index do |char, index|
+      if char == correct_answer[index]
+        @correct_letters += 1
+      end
+    end
+    false
+  end
 end
 
 end
