c245cc9a8330d1f9425081572a6248c1a45960f7
Toby Clarke <Toby.Clarke@euromoneyplc.com>
2016-02-04 16:22:55 +0000

    updated test names

--- StringCalculatorTDD/Ruby/spec/string_calculator_tdd_spec.rb
+++ StringCalculatorTDD/Ruby/spec/string_calculator_tdd_spec.rb
@@ -10,11 +10,11 @@ describe StringCalculatorTDD do
       expect(subject.add("")).to eq 0
     end
 
-    it "returns zero when given no numbers" do
+    it "returns 1 when given the string one" do
       expect(subject.add("1")).to eq 1
     end
 
-    it "returns zero when given no numbers" do
+    it "returns 2 when given the string two" do
       expect(subject.add("2")).to eq 2
     end
   end
