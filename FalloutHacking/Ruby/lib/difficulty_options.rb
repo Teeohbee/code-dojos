9c134f76bbe48d42466f479fc014f90040423150
Toby Clarke <teeohbee@gmail.com>
2016-02-08 20:27:16 +0000

    difficulty options properties complete

--- FalloutHacking/Ruby/lib/difficulty_options.rb
+++ FalloutHacking/Ruby/lib/difficulty_options.rb
@@ -1,12 +1,13 @@
 class DifficultyOptions
   attr_reader :word_length
+  attr_reader :word_list_length
 
   DIFFICULTY_WORD_LENGTH = { easy: [3,4,5], medium: [6,7,8], hard: [9,10,11]}
+  DIFFICULTY_WORD_LIST_LENGTH = { easy: [5,6,7,8], medium: [9,10,11,12], hard: [13,14,1516]}
 
   def set_difficulty(difficulty_selection)
-    @word_length = DIFFICULTY_WORD_LENGTH[difficulty_selection].sample
-
-
+    @word_length = DIFFICULTY_WORD_LENGTH[difficulty_selection.to_sym].sample
+    @word_list_length = DIFFICULTY_WORD_LIST_LENGTH[difficulty_selection.to_sym].sample
   end
 
 end
--- FalloutHacking/Ruby/spec/difficulty_options_spec.rb
+++ FalloutHacking/Ruby/spec/difficulty_options_spec.rb
@@ -7,8 +7,13 @@ describe DifficultyOptions do
     it { is_expected.to respond_to(:set_difficulty).with(1).arguments }
 
     it 'updates the word length property' do
-      subject.set_difficulty(:medium)
+      subject.set_difficulty('medium')
       expect(subject.word_length).to be_between(6, 8).inclusive
+    end
+
+    it 'updates the word list length property' do
+      subject.set_difficulty('medium')
+      expect(subject.word_list_length).to be_between(9, 12).inclusive
     end
   end
 end
