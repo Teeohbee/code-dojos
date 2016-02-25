0e9cc00c0909e5fd0b36edaae378f57b16f4891e
Toby Clarke <Toby.Clarke@euromoneyplc.com>
2016-02-25 15:11:27 +0000

    New kata - A game of threes

---
+++ GameOfThrees/C#/GameOfThrees/GameOfThrees.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfThrees
+{
+    public class GameOfThrees
+    {
+        public int Play(int inputNumber)
+        {
+            return 1;
+        }
+    }
+}
---
+++ GameOfThrees/C#/GameOfThrees/GameOfThreesTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GameOfThrees
+{
+    [TestFixture]
+    class GameOfThreesTests
+    {
+        public GameOfThrees Game { get; set; }
+        [SetUp]
+        public void Init()
+        {
+            Game = new GameOfThrees();
+
+        }
+        [Test]
+        public void ShouldReturnZeroWhenGivenZero()
+        {
+            Assert.AreEqual(0, Game.Play(0));
+        }
+        [Test]
+        public void ShouldReturnOneWhenGivenThree()
+        {
+            Assert.AreEqual(1, Game.Play(3));
+        }
+    }
+}
