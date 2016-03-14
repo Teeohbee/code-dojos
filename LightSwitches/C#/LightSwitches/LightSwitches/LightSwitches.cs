13a4bf62969596279c0fc7c6c4f058fff20e0cd1
Toby Clarke <Toby.Clarke@euromoneyplc.com>
2016-03-14 16:36:20 +0000

    initial test passing

---
+++ LightSwitches/C#/LightSwitches/LightSwitches/LightSwitches.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LightSwitches
+{
+    public class LightSwitches
+    {
+        public int Lights { get; set; }
+    }
+
+    [TestFixture]
+    class LightSwitchesTests
+    {
+        public LightSwitches LightSwitchGame;
+        [Test]
+        public void ShouldInitialiseWithANumberofLightsProperty()
+        {
+            LightSwitchGame = new LightSwitches();
+            Assert.That(LightSwitchGame, Has.Property("Lights"));
+        }
+    }
+}
