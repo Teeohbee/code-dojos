b147b9d9a854486b9b825ac1560ade62579a1e3d
Toby Clarke <Toby.Clarke@euromoneyplc.com>
2016-03-14 17:24:51 +0000

    Added turn on lights function and tests

--- LightSwitches/C#/LightSwitches/LightSwitches/LightSwitches.cs
+++ LightSwitches/C#/LightSwitches/LightSwitches/LightSwitches.cs
@@ -21,6 +21,25 @@ namespace LightSwitches
                 LightStatus.Add(0);
             }
         }
+
+        public void TurnOnLights(string input)
+        {
+            List<int> lights = input.Split(' ').Select(Int32.Parse).ToList();
+            int startingLight = lights[0];
+            int count = lights[1] - lights[0] + 1;
+            IEnumerable<int> lightRange = Enumerable.Range(startingLight, count);
+            foreach (int index in lightRange)
+            {
+                if (LightStatus[index] == 0)
+                {
+                    LightStatus[index] = 1;
+                }
+                else if (LightStatus[index] == 1)
+                {
+                    LightStatus[index] = 0;
+                }
+            }
+        }
     }
 
     [TestFixture]
@@ -49,6 +68,15 @@ namespace LightSwitches
             LightSwitchGame.NoOfLights = 10;
             LightSwitchGame.GenerateStatusArray();
             var list = new List<int> { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            Assert.AreEqual(list, LightSwitchGame.LightStatus);
+        }
+        [Test]
+        public void ShouldTurnOnFirstLightWhenGivenZeroZero()
+        {
+            LightSwitchGame.NoOfLights = 10;
+            LightSwitchGame.GenerateStatusArray();
+            var list = new List<int> { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            LightSwitchGame.TurnOnLights("0 0");
             Assert.AreEqual(list, LightSwitchGame.LightStatus);
         }
     }
