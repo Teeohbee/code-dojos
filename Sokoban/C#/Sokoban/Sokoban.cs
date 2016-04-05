9211401f2f1b07635f8595675a8114d653c5e8af
Toby Clarke <Toby.Clarke@euromoneyplc.com>
2016-04-05 17:21:24 +0100

    Added lunch time code dojo effort

---
+++ Sokoban/C#/Sokoban/Sokoban.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Sokoban
+{
+    public class Sokoban
+    {
+        public List<string> Grid { get; set; }
+
+        public const char Man = '@';
+        public const char Crate = 'o';
+        public const char Storage = '.';
+        public const char CrateOnStorage = '*';
+        public const char ManOnStorage = '+';
+
+
+
+        public Sokoban(int x, int y)
+        {
+            Grid = new List<string>();
+            Grid.Add(("#####"));
+            Grid.Add(("#   #"));
+            Grid.Add(("#####"));
+        }
+
+        public void PlaceMan(int x, int y)
+        {
+            Grid[y] = Grid[y].Remove(x, 1).Insert(x, Man.ToString());
+        }
+
+        public void Move(char c)
+        {
+            var positionOfMan = Grid[1].IndexOf(Man.ToString());
+
+            Grid[1] = Grid[1].Remove(positionOfMan, 1).Insert(positionOfMan, " ");
+            if (Grid[1][positionOfMan - 1] == Storage)
+            {
+                Grid[1] = Grid[1].Remove(positionOfMan - 1, 1).Insert(positionOfMan - 1, ManOnStorage.ToString());
+
+            }
+            if (Grid[1][positionOfMan - 1] == Crate)
+            {
+                if (Grid[1][positionOfMan - 2] == Storage)
+                {
+                    Grid[1] = Grid[1].Remove(positionOfMan - 2, 1).Insert(positionOfMan - 2, CrateOnStorage.ToString());
+                }
+                else
+                {
+
+                    Grid[1] = Grid[1].Remove(positionOfMan - 2, 1).Insert(positionOfMan - 2, Crate.ToString());
+                }
+            }
+            Grid[1] = Grid[1].Remove(positionOfMan-1, 1).Insert(positionOfMan-1, Man.ToString());
+        }
+
+        public void PlaceCrate(int x, int y)
+        {
+            Grid[y] = Grid[y].Remove(x, 1).Insert(x, Crate.ToString());
+        }
+
+        public void PlaceStorage(int x, int y)
+        {
+            Grid[y] = Grid[y].Remove(x, 1).Insert(x, Storage.ToString());
+        }
+    }
+}
---
+++ Sokoban/C#/Sokoban/SokobanTests.cs
@@ -0,0 +1,122 @@
+﻿using System.Net.NetworkInformation;
+using NUnit.Framework;
+
+namespace Sokoban
+{
+    [TestFixture]
+    public class SokobanTests
+    {
+        [Test]
+        public void GridIsCreatedFromGiven5By3Size()
+        {
+            var sokoban = new Sokoban(5, 3);
+
+            Assert.That(sokoban.Grid[0], Is.EqualTo("#####"));
+            Assert.That(sokoban.Grid[1], Is.EqualTo("#   #"));
+            Assert.That(sokoban.Grid[2], Is.EqualTo("#####"));
+        }
+
+        [Test]
+        public void PlaceManOnBoardPlacesMan()
+        {
+            var sokoban = new Sokoban(5, 3);
+            sokoban.PlaceMan(3, 1);
+
+            Assert.That(sokoban.Grid[1], Is.EqualTo("#  @#"));
+        }
+
+        [Test]
+        public void PlaceManOnBoardOn21PlacesManAt21()
+        {
+            var sokoban = new Sokoban(5, 3);
+            sokoban.PlaceMan(2, 1);
+
+            Assert.That(sokoban.Grid[1], Is.EqualTo("# @ #"));
+        }
+
+        [Test]
+        public void MovingLeftFrom31MovesManLeftTo21()
+        {
+            var sokoban = new Sokoban(5, 3);
+            sokoban.PlaceMan(3, 1);
+
+            sokoban.Move('A');
+            Assert.That(sokoban.Grid[1], Is.EqualTo("# @ #"));
+        }
+
+        [Test]
+        public void PlaceCrateOnBoardOn11PlacesManAt11()
+        {
+            var sokoban = new Sokoban(5, 3);
+            sokoban.PlaceCrate(1, 1);
+
+            Assert.That(sokoban.Grid[1], Is.EqualTo("#o  #"));
+        }
+
+        [Test]
+        public void MovingLeftFrom21MovesManLeftTo11()
+        {
+            var sokoban = new Sokoban(5, 3);
+            sokoban.PlaceMan(2, 1);
+
+            sokoban.Move('A');
+            Assert.That(sokoban.Grid[1], Is.EqualTo("#@  #"));
+        }
+
+        [Test]
+        public void MovingManLeftFrom31NearCrateMovesManLeftTo21AndCrateTo11()
+        {
+            var sokoban = new Sokoban(5, 3);
+            sokoban.PlaceMan(3, 1);
+            sokoban.PlaceCrate(2, 1);
+
+            sokoban.Move('A');
+            Assert.That(sokoban.Grid[1], Is.EqualTo("#o@ #"));
+        }
+
+        [Test]
+        public void PlaceStorageOnBoardOn11PlacesStorageAt11()
+        {
+            var sokoban = new Sokoban(5, 3);
+
+            sokoban.PlaceStorage(1, 1);
+
+            Assert.That(sokoban.Grid[1], Is.EqualTo("#.  #"));
+        }
+
+        [Test]
+        public void MovingCrateOntoStorageShowsCrateOnStorage()
+        {
+            var sokoban = new Sokoban(5, 3);
+            sokoban.PlaceMan(3, 1);
+            sokoban.PlaceCrate(2, 1);
+            sokoban.PlaceStorage(1, 1);
+
+            sokoban.Move('A');
+            Assert.That(sokoban.Grid[1], Is.EqualTo("#*@ #"));
+        }
+
+        [Test]
+        public void MovingManOntoStorageShowsManOnStorage()
+        {
+            var sokoban = new Sokoban(5, 3);
+            sokoban.PlaceMan(3, 1);
+            sokoban.PlaceStorage(2, 1);
+
+            sokoban.Move('A');
+            Assert.That(sokoban.Grid[1], Is.EqualTo("# + #"));
+        }
+
+        [Test]
+        public void MovingManIntoWallDoesntMoveMan()
+        {
+            var sokoban = new Sokoban(5, 3);
+            sokoban.PlaceMan(1, 1);
+
+            sokoban.Move('A');
+            Assert.That(sokoban.Grid[1], Is.EqualTo("#@  #"));
+        }
+
+
+    }
+}
